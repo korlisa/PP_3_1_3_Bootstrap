1a230bca89d625cfb062f11c531397327b62238e
Lisa <1lizacorobova1@gmail.com>
2023-02-02 13:57:02 +0500

    commit2

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -103,7 +103,7 @@ public class AdminController {
         user.setLastName(user.getLastName());
         user.setAge(user.getAge());
         user.setUsername(user.getUsername());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRoles(roles);
         userService.createOrUpdate(user);
         return "redirect:/admin";
--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package ru.kata.spring.boot_security.demo.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
+import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername (String username);
+    @EntityGraph(attributePaths = {"roles"})
+    User findByUsername(String username);
+
+
+    @EntityGraph(attributePaths = {"roles"})
+    List<User> findAll();
+
+    @EntityGraph(attributePaths = {"roles"})
+    Optional<User> findById(Long id);
 }
